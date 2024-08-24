90b5a037b5753cc7052651e2aad17b2a924f8bdf
nd <nd1012@users.noreply.github.com>
2024-08-24 11:20:11 +0700

    Remove Aspire projects

--- src/wan24-AutoDiscover.AppHost/Program.cs
+++
@@ -1,7 +0,0 @@
-using Aspire.Hosting;
-
-var builder = DistributedApplication.CreateBuilder(args);
-
-builder.AddProject<Projects.wan24_AutoDiscover>("wan24-autodiscover");
-
-builder.Build().Run();
